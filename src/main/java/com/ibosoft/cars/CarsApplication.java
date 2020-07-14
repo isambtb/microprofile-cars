a11ca1b26a86f82d53cbdf68a314342a1c59a184
isambtb <isambtb@gmail.com>
2020-07-14 23:28:40 +0200

    [Microprofile][IB] add api version

--- src/main/java/com/ibosoft/cars/CarsApplication.java
+++ src/main/java/com/ibosoft/cars/CarsApplication.java
@@ -3,7 +3,7 @@ package com.ibosoft.cars;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/car")
+@ApplicationPath("/api/${api.version}")
 public class CarsApplication  extends Application {
 
 }
